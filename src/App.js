bae6e5847370f051fafa86d61704b44fe28b4fe6
Akshar Gabani <akshargabanistar945@gmail.com>
2023-10-18 09:03:59 +0530

    form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    isLoggedIn: false,
+    isSigningUp: false,
+    userData: {
+      username: '',
+      password: '',
+    },
+    loginData: {
+      username: 'exampleUser',
+      password: 'examplePassword',
+    },
+    error: '',
+  };
+
+  handleChange = (field, e) => {
+    const { userData } = this.state;
+    this.setState({ userData: { ...userData, [field]: e.target.value }, error: '' });
+  }
+
+  handleAction = () => {
+    const { userData, loginData, isSigningUp } = this.state;
+    const isMatch = userData.username === loginData.username && userData.password === loginData.password;
+    
+    this.setState({ error: isMatch ? '' : 'Username or password is incorrect', isLoggedIn: isMatch && !isSigningUp });
+  }
+
+  handleLogout = () => {
+    this.setState({
+      isLoggedIn: false,
+      userData: { username: '', password: '' },
+      error: '',
+    });
+  }
+
+  render() {
+    const { isLoggedIn, isSigningUp, userData, error } = this.state;
+
+    return (
+      <div>
+        {isLoggedIn ? (
+          <div>
+            <p>Welcome, {userData.username}!</p>
+            <button onClick={this.handleLogout}>Logout</button>
+          </div>
+        ) : (
+          <div>
+            <h2>{isSigningUp ? 'Signup' : 'Login'}</h2>
+            {error && <p style={{ color: 'red' }}>{error}</p>}
+            <input
+              type="text"
+              placeholder="Username"
+              value={userData.username}
+              onChange={(e) => this.handleChange('username', e)}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              value={userData.password}
+              onChange={(e) => this.handleChange('password', e)}
+            />
+            <button onClick={this.handleAction}>
+              {isSigningUp ? 'Signup' : 'Login'}
+            </button>
+            <button onClick={() => this.setState({ isSigningUp: !isSigningUp, error: '' })}>
+              {isSigningUp ? 'Back to Login' : 'Signup'}
+            </button>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
